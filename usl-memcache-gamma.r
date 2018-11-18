999f986379ec4040913ea50be01d56cd1b4ee908
Neil J. Gunther <DrQz@users.noreply.github.com>
2018-11-17 20:37:15 -0800

    Updated with A,B,G

--- usl-memcache-gamma.r
+++ usl-memcache-gamma.r
@@ -15,29 +15,33 @@ head(df.mc)
 plot(df.mc$N,df.mc$X, type="p",
      xlim=c(0,12), ylim=c(0,300),
      main="USL3 model of Memcache using NLS",
-     xlab="Worker threads (N)",ylab="Throughput in KOPS X(N)"
+     xlab="Worker threads (N)",
+     ylab="Throughput in KOPS X(N)"
      )
 
 # Fit the nonlinear USL model
 usl.fit <- nls(X ~ gamma * N / 
                  (1 + alpha * (N - 1) + beta * N * (N - 1)), 
-               data=df.mc, start=list(gamma=100, alpha=1e-2, beta=1e-2),
-               algorithm="port", 
-               lower=c(50,0,0), upper=c(200,5e-1,5e-1))
+               data=df.mc, algorithm="port", 
+               start=list(gamma=100, alpha=1e-2, beta=1e-2),
+               lower=c(50,0,0), upper=c(200,5e-1,5e-1)
+               )
 summary(usl.fit)
 
+A <- coef(usl.fit)['alpha']
+B <- coef(usl.fit)['beta']
+G <- coef(usl.fit)['gamma']
+
 # overlay the USL curve
-curve(coef(usl.fit)['gamma'] * x / (1 + coef(usl.fit)['alpha'] * (x - 1) + 
-                                      coef(usl.fit)['beta'] * x * (x - 1)), 
-      from=0, to=12, add=TRUE, col="blue", lwd=2)
+curve(G * x / (1 + A * (x - 1) +  B * x * (x - 1)), 
+from=0, to=12, add=TRUE, col="blue", lwd=2
+)
 
 # compute USL scaling metrics
-Nmax <- sqrt((1 - coef(usl.fit)['alpha']) / coef(usl.fit)['beta'])
-Xmax <- coef(usl.fit)['gamma'] * Nmax / 
-  (1 + coef(usl.fit)['alpha'] * (Nmax - 1) + 
-     coef(usl.fit)['beta'] * Nmax * (Nmax - 1))
-Nopt <- abs(1 / coef(usl.fit)['alpha'])
-Xroof <- coef(usl.fit)['gamma'] * Nopt 
+Nmax  <- sqrt((1 - A) / B)
+Xmax  <- G * Nmax / (1 + A * (Nmax - 1) + B * Nmax * (Nmax - 1))
+Nopt  <- abs(1 / A)
+Xroof <- G * Nopt 
 
 abline(v=Nmax,col="gray",lty="dashed")
 abline(h=Xmax,col="gray",lty="dashed")
@@ -46,7 +50,6 @@ abline(b=coef(usl.fit)['gamma'], a=0, col="gray")
 legend("bottom",
        legend=eval(parse(text=sprintf("expression(alpha == %.4f, 
       beta == %.6f, gamma == %.2f, Nmax==%.2f, Xmax==%.2f,  Xroof==%.2f)", 
-       coef(usl.fit)['alpha'], coef(usl.fit)['beta'], 
-       coef(usl.fit)['gamma'], Nmax, Xmax, Xroof))), 
+       A, B, G, Nmax, Xmax, Xroof))), 
        ncol=2, inset=0.05, cex=0.75
        )
